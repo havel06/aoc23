3e46a337d58f3b2e66a84410827a5f82c115f930
Michal Havlíček <michalhav7@seznam.cz>
2023-12-04 10:25:13 +0100

    day 4, solution 2

--- 4/main.lisp
+++ 4/main.lisp
@@ -33,11 +33,28 @@
 	(apply '+ 
 		(mapcar 'line-points lines)))
 
+(defun solve2 (lines)
+	(let (
+		(cards (loop for i from 0 below (list-length lines) collect i))
+		(sum 0)
+		(len (list-length lines))
+		(processed-lines (mapcar 'matching-numbers lines)))
+			(loop while cards do
+				(let ((card (first cards)))
+					(setf cards (cdr cards))
+					(when (< card len)
+						(setf sum (+ 1 sum))
+						(let ((new-cards
+							(loop for i from 1 to (nth card processed-lines) collect (+ card i))))
+								(when new-cards
+									(setf cards (append new-cards cards)))))))
+		sum))
+
 (let (lines (list))
 	(loop
 		(let ((line (read-line *standard-input* nil nil))) (progn
 			(when (eq line nil) (return))
 			(setf lines (append lines (list line))))))
 
-	(print (solve1 lines)))
-	;(print (solve2 lines)))
+	;(print (solve1 lines)))
+	(print (solve2 lines)))
